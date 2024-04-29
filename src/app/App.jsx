185fa7e93ec7605ba0bd9293dc0968a1b08d3442
togay-aytemiz <togayaytemiz@gmail.com>
2024-04-29 20:38:25 +0300

    add red rectangle

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,9 +4,19 @@ import ImageComp from "./components/ImageComp";
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center gap-5">
-      <LoginForm  />
-      <ImageComp />
+    <div className="relative">
+
+      <div className="flex items-center justify-center gap-5 mt-20 relative">
+        <LoginForm  />
+        <ImageComp />
+      </div>
+
+      <img
+         src="/assets/rectangle.png"
+         alt="red"
+         style={{ width: '649.33px', height: '1143.68px', position: 'absolute', top:'-110px', right: '0', zIndex: '-1' }}
+      />
+
     </div>
   );
 };
